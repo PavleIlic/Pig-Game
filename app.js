78ff23135424341bf693775b4e1a02ad7501501b
Pavle Ilic <pavle.ilic24d@gmail.com>
2020-07-27 16:29:37 +0200

    creating new branch and push modified app.js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
-roundScores = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 
 
 document.querySelector(".dice").style.display = "none";
@@ -33,7 +33,32 @@ document.querySelector(".btn-roll").addEventListener('click', function () {
     var diceDOM = document.querySelector(".dice");
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-    
+
     // Update round score IF the rolled number was not 1
+    
+    if(dice !== 1){
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else {
+        // Move to next player
+        roundScore = 0;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+    }
 })
 
